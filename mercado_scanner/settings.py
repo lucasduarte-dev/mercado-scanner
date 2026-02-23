621dbbb0cd86e010a2c9d412dc9a86dcf92bd479
lucasduarte-dev <lucasduarte.software@gmail.com>
2026-02-23 15:41:08 -0300

    Ultima version 23/2 settings

--- mercado_scanner/settings.py
+++ mercado_scanner/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://phagedaenic-deprecatively-vallie.ngrok-free.dev",
+    'https://*.ngrok.io',
 ]
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
